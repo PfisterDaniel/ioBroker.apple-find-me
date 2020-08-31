0ef468d81cfc0a22e6e7f89cdca5fa0a2d1c532c
Daniel Pfister <kontakt@daniel-pfister.de>
2020-08-31 23:32:56 +0200

    Initial Commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "find-me adapter settings": {
+        "en": "Adapter settings for find-me",
+        "de": "Adaptereinstellungen für find-me",
+        "ru": "Настройки адаптера для find-me",
+        "pt": "Configurações do adaptador para find-me",
+        "nl": "Adapterinstellingen voor find-me",
+        "fr": "Paramètres d'adaptateur pour find-me",
+        "it": "Impostazioni dell'adattatore per find-me",
+        "es": "Ajustes del adaptador para find-me",
+        "pl": "Ustawienia adaptera dla find-me",
+        "zh-cn": "find-me的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
